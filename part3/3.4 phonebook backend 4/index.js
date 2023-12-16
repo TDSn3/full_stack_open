cc8ee6f668ae4bcd01b6f492554d04c0d41d0b1e
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-12-16 08:32:24 +0100

    part3: 3.4

---
+++ part3/3.4 phonebook backend 4/index.js
@@ -0,0 +1,69 @@
+
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+
+let persons = [
+    { 
+        id: 1,
+        name: "Arto Hellas", 
+        number: "040-123456"
+    },
+    { 
+        id: 2,
+        name: "Ada Lovelace", 
+        number: "39-44-5323523"
+    },
+    { 
+        id: 3,
+        name: "Dan Abramov", 
+        number: "12-43-234345"
+    },
+    { 
+        id: 4,
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122"
+    }
+]
+
+const personsSize = () => {
+    const size = persons.reduce( (accumulator) => {
+        return (accumulator + 1)
+    }, 0)
+    
+    return (size)
+}
+
+app.use(express.json())
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${personsSize()} people</p><p>${new Date()}</p>`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find((personValue) => (personValue.id === id))
+
+    if (person)
+        response.json(person)
+    else
+        response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter((personValue) => (personValue.id !== id))
+
+    response.status(204).end()
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
