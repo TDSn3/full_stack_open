86d12825463af6ffae4156d41135dc5c1f5623e7
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-08 20:05:42 +0100

    part3: ex 3.10 commit for Render.com - test

--- part3/3.10 phonebook backend 10/backend/index.js
+++ part3/3.10 phonebook backend 10/backend/index.js
@@ -6,9 +6,9 @@ const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
 
 app.use(cors())
 
