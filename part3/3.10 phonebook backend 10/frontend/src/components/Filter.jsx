dd00ce536715b5938d0fc893c8b2d971fb78444a
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-08 19:38:04 +0100

    part3: ex 3.10 commit for Render.com

---
+++ part3/3.10 phonebook backend 10/frontend/src/components/Filter.jsx
@@ -0,0 +1,10 @@
+
+const Filter = ({ newFilter, handlenewFilterChange }) =>
+{	
+	return (
+		<>
+			filter show with <input value={newFilter} onChange={handlenewFilterChange} />
+		</>
+	)}
+
+export default Filter
