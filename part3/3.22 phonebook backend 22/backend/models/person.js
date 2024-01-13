1c11f0888bbc5bc963656e59f22b059935114b6e
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-13 15:42:00 +0100

    part3: ex 3.22 Add Eslint

---
+++ part3/3.22 phonebook backend 22/backend/models/person.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://${process.env.RENDER_USERNAME}:${process.env.RENDER_PASSWORD}@cluster0.eqahmbr.mongodb.net/${process.env.RENDER_DATABASE_NAME}?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+
+console.log('connecting to ', url)
+
+mongoose.connect(url)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+function validateNumber(number) {
+    const numberRegex = /^\d{2,3}-\d+$/
+
+    if (!numberRegex.test(number)) {
+        return (false)
+    }
+
+    return (true)
+}
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: validateNumber,
+            message: (props) => `${props.value} is not a valid number!`,
+        },
+    },
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        const modifiedObject = { ...returnedObject, id: returnedObject._id.toString() }
+        delete modifiedObject._id
+        delete modifiedObject.__v
+        return modifiedObject
+    },
+})
+
+module.exports = mongoose.model('Person', personSchema)
