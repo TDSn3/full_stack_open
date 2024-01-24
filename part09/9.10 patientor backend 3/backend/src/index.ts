5b43c3d534ff7366ac876e00c51a9684ec5765a7
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-24 11:59:45 +0100

    part9: ex 9.10

---
+++ part09/9.10 patientor backend 3/backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import diagnosesRoute from './routes/diagnoses';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+  console.log('GET \'/api/patients\'');
+  res.send('');
+});
+
+app.use('/api/diagnoses', diagnosesRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
