471b79fe0875a4fec51e7943c476eb2ceb11ddca
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-29 16:08:30 +0100

    part9: ex 9.16 end

--- part09/9.16 flight-diary/frontend/src/App.tsx
+++ part09/9.16 flight-diary/frontend/src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Diary } from './types';
+import DiaryComponent from './DiaryComponent';
+import { getAllDiaries } from './diaryService';
+
 const App = () => {
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+
+  const hook = () => {
+    getAllDiaries().then((data) => setDiaries(data));
+  };
+
+  useEffect(hook, []);
+
   return (
     <div>
+      {
+        diaries.map((diaryValue) => <DiaryComponent key={diaryValue.id} diary={diaryValue}/>)
+      }
     </div>
   );
 };
---
+++ part09/9.16 flight-diary/frontend/src/diaryService.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { Diary/*, NewDiary*/ } from './types';
+
+const url = 'http://localhost:3000/api/diaries';
+
+export const getAllDiaries = () => {
+	return (
+		axios
+			.get<Diary[]>(url)
+			.then((response) => response.data)
+	);
+};
