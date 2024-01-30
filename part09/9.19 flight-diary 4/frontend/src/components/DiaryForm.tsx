7c140abcf4e38c24a95b0e87c00a78b6ee87e126
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-30 17:14:32 +0100

    part9: ex 9.19 end

--- part09/9.19 flight-diary 4/frontend/src/components/DiaryForm.tsx
+++ part09/9.19 flight-diary 4/frontend/src/components/DiaryForm.tsx
@@ -21,17 +21,99 @@ const DiaryForm = (props: DiaryFormProps) =>
 	return (
 			<form onSubmit={props.handleAddDiary}>
 				<div>
-					Date: <input value={props.newDate} onChange={props.handleNewDateOnChange} />
+					Date: <input type="date" value={props.newDate} onChange={props.handleNewDateOnChange} />
 				</div>
 				<div>
-				Visibility: <input value={props.newVisibility} onChange={props.handleNewVisibilityOnChange} />
+					Visibility: 
+					<input
+						type="radio"
+						value='great'
+						onChange={props.handleNewVisibilityOnChange}
+						name="visibility"
+						checked={props.newVisibility === 'great'}
+					/>
+					<label>great</label>
+
+					<input
+						type="radio"
+						value='good'
+						onChange={props.handleNewVisibilityOnChange}
+						name="visibility"
+						checked={props.newVisibility === 'good'}
+					/>
+					<label>good</label>
+
+					<input
+						type="radio"
+						value='ok'
+						onChange={props.handleNewVisibilityOnChange}
+						name="visibility"
+						checked={props.newVisibility === 'ok'}
+					/>
+					<label>ok</label>
+
+					<input
+						type="radio"
+						value='poor'
+						onChange={props.handleNewVisibilityOnChange}
+						name="visibility"
+						checked={props.newVisibility === 'poor'}
+					/>
+					<label>poor</label>
 				</div>
+
 				<div>
-				Weather: <input value={props.newWeather} onChange={props.handleNewWeatherOnChange} />
+					Weather:
+					<input
+						type="radio"
+						value='sunny'
+						onChange={props.handleNewWeatherOnChange}
+						name="weather"
+						checked={props.newWeather === 'sunny'}
+					/>
+					<label>sunny</label>
+
+					<input
+						type="radio"
+						value='rainy'
+						onChange={props.handleNewWeatherOnChange}
+						name="weather"
+						checked={props.newWeather === 'rainy'}
+					/>
+					<label>rainy</label>
+
+					<input
+						type="radio"
+						value='cloudy'
+						onChange={props.handleNewWeatherOnChange}
+						name="weather"
+						checked={props.newWeather === 'cloudy'}
+					/>
+					<label>cloudy</label>
+
+					<input
+						type="radio"
+						value='stormy'
+						onChange={props.handleNewWeatherOnChange}
+						name="weather"
+						checked={props.newWeather === 'stormy'}
+					/>
+					<label>stormy</label>
+
+					<input
+						type="radio"
+						value='windy'
+						onChange={props.handleNewWeatherOnChange}
+						name="weather"
+						checked={props.newWeather === 'windy'}
+					/>
+					<label>windy</label>
 				</div>
+
 				<div>
-				Comment: <input value={props.newComment} onChange={props.handleNewCommentOnChange} />
+					Comment: <input value={props.newComment} onChange={props.handleNewCommentOnChange} />
 				</div>
+
 				<div>
 					<button type="submit">add</button>
 				</div>
