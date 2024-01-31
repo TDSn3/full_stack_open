a38e2c41f388bf4d91928683e3147ec641353aaf
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-01-31 15:27:23 +0100

    part9: ex 9.20 add backend

---
+++ part09/9.20 patientor 1/backend/src/utils/types.ts
@@ -0,0 +1,24 @@
+export interface Diagnostic {
+	code: string,
+	name: string,
+	latin?: string,
+}
+
+export enum Gender {
+	Male = 'male',
+	Female = 'female',
+	Other = 'other',
+}
+
+export interface Patient {
+	id: string,
+	name: string,
+	dateOfBirth: string,
+	ssn: string,
+	gender: string,
+	occupation: string,
+}
+
+export type NewPatient = Omit<Patient, 'id'>;
+
+export type PatientNonSensitiveEntries = Omit<Patient, 'ssn'>;
