a6e5fba593b2d062936d7a3f3438a80afafc464d
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-02-02 16:53:49 +0100

    part9: ex 9.21 bug fixed. use state for recup data from backend in component

--- part09/9.21 patientor 2/frontend/src/components/PatientPage.tsx
+++ part09/9.21 patientor 2/frontend/src/components/PatientPage.tsx
@@ -1,18 +1,27 @@
+import { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
-import { Patient } from "../types";
+import patientService from '../services/patients';
+import { Patient } from '../types';
 
 const PatientPage = () => {
+	const [patient, setPatient] = useState<Patient | null>(null);
 	const { state } = useLocation();
-	const patient: Patient = state.patient;
 
-	return (
-		<>
-			<h1>{patient.name}</h1>
-			<p>gender: {patient.gender}</p>
-			<p>ssn: {patient.ssn}</p>
-			<p>occupation: {patient.occupation}</p>
-		</>
-	);
+	patientService.getPatient(state.patient.id)
+		.then((data) => { setPatient(data); });
+
+	if (patient) {
+		return (
+			<>
+				<h1>{patient.name}</h1>
+				<p>gender: {patient.gender}</p>
+				<p>ssn: {patient.ssn}</p>
+				<p>occupation: {patient.occupation}</p>
+			</>
+		);
+	} else {
+		return (<></>);
+	}
 };
 
 export default PatientPage;
