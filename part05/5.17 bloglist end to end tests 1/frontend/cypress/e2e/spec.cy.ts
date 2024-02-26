003f484299c8867f6fcfc84745db36012b94a214
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-02-26 20:15:51 +0100

    part5: ex 5.17 start

---
+++ part05/5.17 bloglist end to end tests 1/frontend/cypress/e2e/spec.cy.ts
@@ -0,0 +1,7 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+describe('Note app', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5173');
+  });
+});
